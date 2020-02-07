48bb68399a4a5c6e04a6c219bf8f091f21bf3ae7
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-02-07 14:44:57 +0700

    edit

---
+++ src/controller/feature/leave.js
@@ -0,0 +1,17 @@
+module.exports.leave = {
+    command: 'leave',
+    description: 'command for bot to leave grup',
+    method: async context => {
+        const axios = require('axios')
+        console.log('info')
+        context.send([{
+            "type": "text",
+            "text": "Terimakasih sudah mempercayakan kami untuk membantu kamu dalam menyimpan tugas2 mu, mohon maaf jika kami kurang membantu"
+        },{ 
+          "type": "text", 
+          "text": "Jangan sungkan-sungkan untuk add kami kembali ya"
+        }])
+        context.leave()
+    },  
+};
+  
