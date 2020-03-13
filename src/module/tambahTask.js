2855945829ff66e2a107465c15b08bccd03ee7cb
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-03-13 09:08:59 +0700

    edit

--- src/module/tambahTask.js
+++ src/module/tambahTask.js
@@ -1,20 +1,20 @@
 exports.tambahTask = (req) => {
-    console.log("Test")
+    // console.log("Test")
     const payload = req.body;
-    console.log(payload)
-    // const MongoClient = require('mongodb').MongoClient;
-    // const uri = `mongodb+srv://adminbot:${process.env.PASSWORD_MONGO_DB}@bot-linecoba-78zrv.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-    // const client = new MongoClient(uri, { useNewUrlParser: true });
-    // await client.connect(async err => {
-    //     const collection = client.db("bot-dicoding").collection(payload.userId);
-    //     let idx = await collection.count()
-    //     console.log(idx)            
-    //     const myobj = { id: 'ID'+(idx+1),status: true, task: text };
-    //     console.log(myobj)            
-    //     await collection.insertOne(myobj, function(err, res) {
-    //         if (err) throw err;
-    //         console.log("1 document inserted"); 
-    //         client.close();
-    //     })
-    // });
+    // console.log(payload)
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = `mongodb+srv://adminbot:${process.env.PASSWORD_MONGO_DB}@bot-linecoba-78zrv.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    await client.connect(async err => {
+        const collection = client.db("bot-dicoding").collection(payload.userId);
+        let idx = await collection.count()
+        console.log(idx)            
+        const myobj = { id: 'ID'+(idx+1),status: true, task: payload.Task };
+        // console.log(myobj)            
+        await collection.insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted"); 
+            client.close();
+        })
+    });
 }
