d38e8439ca182bb345cd23f1dc4257cd537190ad
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-03-18 15:18:59 +0700

    edit

--- src/module/check.js
+++ src/module/check.js
@@ -2,8 +2,7 @@ const { LineClient } = require('messaging-api-line');
 require('dotenv').config();
 
 exports.check = async () =>{
-
   const client = LineClient.connect(process.env.LINE_ACCESS_TOKEN);
   const userId = 'U29ec2e9ed584a03fdccc334571850775'
-  client.pushText(userId, 'Goodbye LINE');
+  client.pushText(userId, 'Test aja nih ehe');
 }
